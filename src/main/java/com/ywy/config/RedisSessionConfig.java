7ac831d1551b076041498fa158ae29c7b010908b
chenshuiyong <Csy7075633>
2018-12-11 17:33:48 +0800

    推荐列表

---
+++ src/main/java/com/ywy/config/RedisSessionConfig.java
@@ -0,0 +1,9 @@
+package com.ywy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = -1)
+public class RedisSessionConfig {
+}
